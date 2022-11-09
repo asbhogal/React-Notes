8965f28837600993d283c3d69370ff051d1813bb
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-09 23:48:16 +0000

    added course notes

---
+++ pass-props-to-a-stateless-functional-component.jsx
@@ -0,0 +1,27 @@
+// There are Calendar and CurrentDate components in the code editor. When rendering CurrentDate from the Calendar component, pass in a property of date assigned to the current date from JavaScript's Date object. Then access this prop in the CurrentDate component, showing its value within the p tags. Note that for prop values to be evaluated as JavaScript, they must be enclosed in curly brackets, for instance date={Date()}.
+
+const CurrentDate = (props) => {
+  return (
+    <div>
+      { /* Change code below this line */ }
+      <p>The current date is: {props.date}</p>
+      { /* Change code above this line */ }
+    </div>
+  );
+};
+
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>What date is it?</h3>
+        { /* Change code below this line */ }
+        <CurrentDate date={Date()}/>
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
